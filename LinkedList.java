2cfc28cb3911d865ffeac3b73ec936e326655bdd
sachchidanand2005 <sachcidanandrajbhar91@gmail.com>
2026-06-12 22:19:41 +0530

    first commit

--- LinkedList.java
+++ LinkedList.java
@@ -95,18 +95,58 @@ public class LinkedList {
         size--;
         return val;
      }
+     public int itrSearch(int key){
+        Node temp=head;
+        int i=0;
+        while (temp != null) {
+            if(temp.data==key){
+                return i;
+            }
+            temp=temp.next;
+            i++;      
+        }
+        return -1;
+     }
+     public int helper(Node head, int key){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==key){
+            return 0;
+        }
+        int idx=helper(head.next, key);
+        if(idx==-1){
+            return -1;
+        }
+       return idx+1;
+     }
+     public int recSearch(int key){
+        return helper(head, key);
+     }
+     public void reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+        while(curr != null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+
+        }
+        head=prev;
+
+     }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addFirst(2);
         ll.addFirst(1);
         ll.addLast(3);
         ll.addLast(4);
-        ll.add(2, 9);
         ll.print();
-        ll.removeFirst();
-        ll.removeLast();
+        ll.reverse();
         ll.print();
-        //System.out.println(ll.size);
+
 
     }
 }
