4f6ac8ca4750769b276f2238277e7d656f027cab
sachchidanand2005 <sachcidanandrajbhar91@gmail.com>
2026-06-29 21:17:51 +0530

    next greater right element

--- StackB.java
+++ StackB.java
@@ -1,24 +1,30 @@
+
 import java.util.*;
 public class StackB {
 
-    public static String reverseStrring(String str){
-        Stack<Character> s = new Stack<>();
-        int idx=0;
-        while(idx<str.length()){
-            s.push(str.charAt(idx));
-            idx++;
-        }
-        StringBuilder result = new StringBuilder("");
-        while(!s.isEmpty()){
-            result.append(s.pop());
-        }
-        return result.toString();
-
-    }
     public static void main(String[] args) {
-        String str="abc";
-        String reverse=reverseStrring(str);
-        System.out.println(reverse);
+        int arr[]={6,8,1,0,3,1};
+        Stack<Integer> s=new Stack<>();
+        int nextGreater[]=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            //1 while
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
+                s.pop();
+            }
+            //if else
+                if(s.isEmpty()){
+                    nextGreater[i]=-1;
+                }
+                else{
+                    nextGreater[i]=arr[s.peek()];
+                }
+            //push
+            s.push(i);
+        }
+        for(int i=0;i<nextGreater.length;i++){
+        System.out.print(nextGreater[i]+" ");
+        }
+        System.out.println();
         
     }
 }
